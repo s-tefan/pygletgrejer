0ba90a989ae3627aef360898edd013ba1618f210
Stefan_Karlsson <gittest@k-son.se>
2019-12-07 23:13:36 +0100

    ap

--- pyglet_hello.py
+++ pyglet_hello.py
@@ -1,7 +1,64 @@
     #!/usr/bin/env python
 import pyglet
+import math
+
+
+def sinus_curve(x0,y0,length,amp):
+    n_points = 101
+    step = 2*math.pi/(n_points-1)
+    vlist = []
+    clist = []
+    color = [255,0,0]
+    for k in range(n_points):
+        t=k*step
+        x = x0 + int(length * k / n_points)
+        y = y0 + int(amp*math.sin(t))
+        vlist += [x,y]
+        clist += color
+    return pyglet.graphics.vertex_list(n_points, 
+            ('v2i',vlist),
+            ('c3B',clist)
+        )
+    
 
 window = pyglet.window.Window()
+
+
+@window.event
+def on_draw():
+    window.clear()
+    burgaren.blit(0,0)
+    label.draw()
+    pyglet.graphics.draw(2, pyglet.gl.GL_POINTS,
+        ('v2i', (10, 15, 30, 35)))
+    vlist0.draw(pyglet.gl.GL_TRIANGLE_STRIP)
+    vlist1.draw(pyglet.gl.GL_QUADS)
+    scurve.draw(pyglet.gl.GL_LINE_STRIP)
+        
+    pyglet.graphics.draw_indexed(4, pyglet.gl.GL_TRIANGLES,
+        [0, 1, 2, 0, 2, 3],
+        ('v2i', (100, 100,
+             150, 100,
+             150, 150,
+             100, 150))
+    )
+
+@window.event
+def on_key_press(symbol, modifiers):
+    print('A key', str(symbol), 'was pressed')
+    if symbol==pyglet.window.key.G:
+        vlist1.colors[9:]=[0,255,0]
+    elif symbol==pyglet.window.key.B:
+        vlist1.colors[9:]=[0,0,255]
+    elif symbol==pyglet.window.key.R:
+        vlist1.colors[9:]=[255,0,0]
+    elif symbol==pyglet.window.key.K:
+        vlist1.colors[9:]=[0,0,0]
+    elif symbol==pyglet.window.key.W:
+        vlist1.colors[9:]=[255,255,255]
+
+
+
 label = pyglet.text.Label('Hello, world!',
                           font_name='Arial',
                           font_size=36,
@@ -34,28 +91,7 @@ vlist1 = pyglet.graphics.vertex_list(4,
             0,0,255))
     )
 
+scurve = sinus_curve(10,300,300,100)
 
-@window.event
-def on_draw():
-    window.clear()
-    burgaren.blit(0,0)
-    label.draw()
-    pyglet.graphics.draw(2, pyglet.gl.GL_POINTS,
-        ('v2i', (10, 15, 30, 35)))
-    vlist0.draw(pyglet.gl.GL_TRIANGLE_STRIP)
-    vlist1.draw(pyglet.gl.GL_QUADS)
-        
-    pyglet.graphics.draw_indexed(4, pyglet.gl.GL_TRIANGLES,
-        [0, 1, 2, 0, 2, 3],
-        ('v2i', (100, 100,
-             150, 100,
-             150, 150,
-             100, 150))
-    )
-
-@window.event
-def on_key_press(symbol, modifiers):
-    print('A key', str(symbol), 'was pressed')
-    
 
 pyglet.app.run()
