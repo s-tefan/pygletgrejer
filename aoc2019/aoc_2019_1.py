e1231fc00867ff9652a9817d6792e9851ca463b6
Stefan_Karlsson <gittest@k-son.se>
2019-12-08 13:06:43 +0100

    vsc

---
+++ aoc2019/aoc_2019_1.py
@@ -0,0 +1,29 @@
+# adventofcode2019 #1
+# rajraj
+
+import sys
+
+def fuelreq(masses):
+    return sum(map(lambda x: x//3-2, masses))
+
+def fueliterate(mass):
+    fuelsum=0
+    fuel=mass//3-2
+    while fuel>0:
+        fuelsum+=fuel
+        fuel=fuel//3-2
+    return fuelsum
+
+def fuelreq2(masses):
+    return sum(map(fueliterate, masses))
+
+
+mymasses=[]
+for line in sys.stdin:
+    try:
+        mymasses.append(int(line.strip()))
+    except:
+        pass    
+
+print(fuelreq2(mymasses))
+
