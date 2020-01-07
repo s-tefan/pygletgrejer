3ffdb13e3f9ed6a205ad3b06d2590cf470b4033b
Stefan K <40999112+s-tefan@users.noreply.github.com>
2020-01-07 19:09:53 +0100

    Update fourierplot.py

--- pygletgrejer/fourierplot.py
+++ pygletgrejer/fourierplot.py
@@ -1,5 +1,4 @@
 import pyglet
-from pyglet.window import mouse
 import cmath
 import time
 import numpy.fft
@@ -17,31 +16,32 @@ twopii = 2j*cmath.pi
 scale = 200
 xoffset = 640
 yoffset = 360
-omega = 1/10
+omega = 1/20
 nt = 100
 tracelist = [0]*(2*nt)
 
-'''
+
 plist = [1,1+0.25j,1+0.5j,1+0.75j, 
     1+1j,0.75+1j,0.5+1j,0.25+1j,0+1j,-0.25+1j,-0.5+1j,-0.75+1j,
     -1+1j,-1+0.75j,-1+0.5j,-1+0.25j,-1+0j,-1-0.25j,-1-0.5j,-1-0.75j,
     -1-1j,-0.75-1j,-0.5-1j,-0.25-1j,0-1j,0.25-1j,0.5-1j,0.75-1j,
     1-1j,1-0.75j,1-0.5j,1-0.25j] # 32 element från en kvadrat
-'''
-plist = [cmath.exp(twopii*k/16)+(random.uniform(-0.25,0.25)+1j*random.uniform(-0.25,0.25)) for k in range(16)]
+
+
+plist = [cmath.exp(twopii*k/16)+(random.uniform(-1,1)+1j*random.uniform(-1,1)) for k in range(16)]
 #flist = numpy.fft.fft(plist,norm = "ortho")
+
+#plist = plist[::4]
+#plist = [0,0.5,1,0.5+0.5j,1j,0.5j]
+#plist = [0,0.5,1,1j,0.5j]
 flist = [c/len(plist) for c in numpy.fft.fft(plist)]
 n = len(flist)//2
 
-
 def poscoeff(n):
-    #return 1j/n + 1/n*cmath.exp(twopii*n/4)
     return flist[n]
 def negcoeff(n):
-    #return -1j/n - 1/n*cmath.exp(twopii*n/4)
     return flist[-n]
 def constcoeff():
-    #return cmath.pi/2
     return flist[0]
 
 
@@ -53,11 +53,15 @@ def blaha(t,n):
     x = xoffset+scale*z.real
     y = yoffset+scale*z.imag
     vlist = [x,y]
-    for k in range(1,n+1):
+    for k in range(1,(n+1)//2):
         z += poscoeff(k)*cmath.exp(twopii*k*t)
         vlist.append(xoffset+scale*z.real)
         vlist.append(yoffset+scale*z.imag)
         z += negcoeff(k)*cmath.exp(-twopii*k*t)
+        vlist.append(xoffset+scale*z.real)
+        vlist.append(yoffset+scale*z.imag)
+    if n%2 == 0:
+        z += poscoeff(n//2)*cmath.exp(twopii*t*(-n//2))
         vlist.append(xoffset+scale*z.real)
         vlist.append(yoffset+scale*z.imag)
     x = xoffset+scale*z.real
@@ -69,40 +73,88 @@ def blaha(t,n):
     tracelist.append(y)
     return vlist, vlistq
 
+
+def blaha2(t,n):
+    z = constcoeff()
+    x = xoffset+scale*z.real
+    y = yoffset+scale*z.imag
+    vlist = [x,y]
+    for k in range(1,n):
+        if k > n//2:
+            m = k-n
+        else:
+            m = k
+        z += poscoeff(k)*cmath.exp(twopii*m*t)
+        vlist.append(xoffset+scale*z.real)
+        vlist.append(yoffset+scale*z.imag)
+    x = xoffset+scale*z.real
+    y = yoffset+scale*z.imag
+    vlistq =  [x+2, y+2, x+2, y-2, x-2, y-2, x-2, y+2]
+    return vlist, vlistq
+
+def blaha3(t,n):
+    vlist = []
+    z = 0
+    ind = list(range(n))
+    if n%2 == 0:
+        ind[1] = n//2
+        for k in range(1,n//2):
+            ind[n//2+1-k] = k
+            ind[n//2+k] = -k 
+    else:
+        for k in range(1,(n+1)//2):
+            ind[n//2-k+1] = k
+            ind[n//2+k] = -k
+    for k in ind:
+        z += poscoeff(k)*cmath.exp(twopii*k*t)
+        vlist.append(xoffset+scale*z.real)
+        vlist.append(yoffset+scale*z.imag)
+    x = xoffset+scale*z.real
+    y = yoffset+scale*z.imag
+    vlistq =  [x+2, y+2, x+2, y-2, x-2, y-2, x-2, y+2]
+    return vlist, vlistq
+
 def update(dt):
-    global vertex_list, quad, trace, tracelist, starttime
+    global vertex_list, vertex_list2, vertex_list3, quad, quad2, trace, tracelist, starttime, p
     t = time.time()-starttime
-    vlists = blaha(omega*t,n)
-    vertex_list = pyglet.graphics.vertex_list(2*n+1, \
+    vlists = blaha(omega*t,len(flist))
+    vlists2 = blaha2(omega*t,len(flist))
+    vlists3 = blaha3(omega*t,len(flist))
+    vertex_list = pyglet.graphics.vertex_list(len(vlists[0])//2, \
         ('v2f', vlists[0] ))
+    vertex_list2 = pyglet.graphics.vertex_list(len(vlists2[0])//2, \
+        ('v2f', vlists2[0] ))
+    vertex_list3 = pyglet.graphics.vertex_list(len(vlists3[0])//2, \
+        ('v2f', vlists3[0] ))
     quad = pyglet.graphics.vertex_list(4, \
         ('v2f', vlists[1]),('c3B', (255,0,0)*4))
-    trace = pyglet.graphics.vertex_list(nt, \
+    quad2 = pyglet.graphics.vertex_list(4, \
+        ('v2f', vlists2[1]),('c3B', (0,255,0)*4))
+    trace = pyglet.graphics.vertex_list(len(tracelist)//2, \
         ('v2f', tracelist),('c3B', (255,0,0)*nt))
 
 starttime = time.time()
 pyglet.clock.schedule_interval(update, 1/30)
 win = pyglet.window.Window(1280,720)
-vlists = blaha(0,n)
 pvlist = []
 for z in plist:
     pvlist += [xoffset+scale*z.real,yoffset+scale*z.imag]
-vertex_list = pyglet.graphics.vertex_list(2*n+1, \
-    ('v2f', vlists[0] ))
-quad = pyglet.graphics.vertex_list(4, \
-    ('v2f', vlists[1]),('c3B', (255,0,0)*4)  )
-trace = pyglet.graphics.vertex_list(nt, \
-    ('v2f', tracelist),('c3B', (255,0,0)*nt))
 p = pyglet.graphics.vertex_list(len(plist), \
     ('v2f', pvlist),('c3B', (0,255,255)*len(plist)))
+update(0)
+
+
 
 
 @win.event
 def on_draw():
-    global vertex_list, quad, trace, p
+    global vertex_list, vertex_list2, vertex_list3, quad, quad2, trace, p
     win.clear()
     vertex_list.draw(pyglet.gl.GL_LINE_STRIP)
+    vertex_list2.draw(pyglet.gl.GL_LINE_STRIP)
+    vertex_list3.draw(pyglet.gl.GL_LINE_STRIP)
     quad.draw(pyglet.gl.GL_QUADS)
+    quad2.draw(pyglet.gl.GL_QUADS)
     trace.draw(pyglet.gl.GL_LINE_STRIP)
     p.draw(pyglet.gl.GL_POINTS)
 
