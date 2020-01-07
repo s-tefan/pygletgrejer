8b91d97f337c2f61c8d6f332028cf28c35ffb620
Stefan_Karlsson <gittest@k-son.se>
2020-01-07 23:25:12 +0100

    fourierplotter worked on, not ready at all

--- fourierplotter.py
+++ fourierplotter.py
@@ -1,34 +1,66 @@
 import cmath
 import numpy.fft
+import time
 
 pi2i = 2*cmath.pi*1j
 
 class Fourierplotter:
-    def __init__(self, flist=[])
+    def __init__(self, flist=[]):
         self.flist = flist
+        self.mode = None
+        self.omega = 1
     
-    def termlist(self.t):
+    def termlist(self, t):
         n = len(flist)
-        return [exp(pi2i*k/n)*self.flist[k] for k in range(n)]
+        return [exp(pi2i*((k+(n//2))%n-(n//2))*t)*self.flist[k] for k in range(n)]
     
-    def setpoints(self.plist):
+    def setpoints(self, plist):
         flistraw = numpy.fft.fft(plist)
         n = len(flistraw)
         self.flist = [f/n for f in flistraw]
+
+    def set_mode(mode):
+        self.mode = mode
     
-    def hands_vertexlist(self, t, scale = 1, xoffset = 0, yoffset = 0):
+    def start(self):
+        self.starttime = time.time()
+    
+    def hands_vertexlist(self, t, mode=0):
         tlist = self.termlist(t)
-        z = tlist[0]
-        zlist = [z]
         n = len(tlist)
-        for k in range(1,n//2): #?
-            z += tlist[k]
-            zlist.append(z)
-            z += tlist[n-k]
-            zlist.append(z)
-        if n%2:
-            z += tlist[n//2]
-            zlist.append(z)
+        if self.mode == 1:
+            # lowest frequency first, sum of counterclockwise and clockwise, 
+            z = tlist[0]
+            zlist = [z]
+            for k in range(1,n//2): #?
+                z += tlist[k]+tlist[-k]
+                zlist.append(z)
+            if n%2:
+                z += tlist[n//2]
+                zlist.append(z)
+        else:
+            # lowest frequency first, counterclockwise, clockwise, 
+            z = tlist[0]
+            zlist = [z]
+            for k in range(1,n//2): #?
+                z += tlist[k]
+                zlist.append(z)
+                z += tlist[-k]
+                zlist.append(z)
+            if n%2:
+                z += tlist[n//2]
+                zlist.append(z)
         return zlist
+
+class FourierAnimator:
+
+    def __init__(self, width = 1280, heigth = 720):
+        self.starttime = time.time()
+        win = pyglet.window.Window(width,height)
+
+    def add_fplotter(self, fplotter, scale = 1, xoffset = 0, yoffset = 0):
+        pass
+
+
 
 
