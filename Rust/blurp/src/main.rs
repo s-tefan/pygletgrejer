e9b2c3b939712fa5869a4b48f0c51fb40cd8401d
Stefan_Karlsson <gittest@k-son.se>
2020-12-05 20:31:18 +0100

    -

--- Rust/blurp/src/main.rs
+++ Rust/blurp/src/main.rs
@@ -1,3 +1,20 @@
+
+fn apa(x: char) -> String {
+    return String::from(x)
+}
+
 fn main() {
     println!("Hello, world!");
+    let x = 23;
+    println!("{}",x);
+    let x = x + 42;
+    println!("{}",x);
+    let mut y = 5;
+    println!("{}",y);
+    y = 7;
+    println!("{}",y);
+    println!("{}", apa('a'));
+    println!("🗻∈🌏");
+    
+
 }
